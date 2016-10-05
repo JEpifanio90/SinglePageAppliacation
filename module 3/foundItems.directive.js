fec18f5f2f43f5d794aceec9e561751ff052a3e0
José <jose.epifanio90@gmail.com>
2016-10-04 22:33:37 -0500

    ugh.

--- module 3/foundItems.directive.js
+++ module 3/foundItems.directive.js
@@ -11,7 +11,7 @@
     {
         return {
             restrict: 'E',
-            templateUrl: '/loader/itemsloaderindicator.template.html',
+            templateUrl: 'loader/itemsloaderindicator.template.html',
             scope: {
                 foundItems : '<',
                 onRemove : '=?'
